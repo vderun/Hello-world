32901854947b5c315630171c28b3e8ff4d00379a
FranPadilla175 <46796703+FranPadilla175@users.noreply.github.com>
2019-01-17 15:40:38 -0600

    Added name, Major, quotation marrks

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 /**
- * Author: Joel, Vadim & BEN
+ * Author: Joel, Vadim & BEN & Francisco Padilla 
  * Date: 2016/11/02
  *
  * A simple hello world program in C
@@ -12,6 +12,9 @@ int main(int argc, char **argv) {
 
   printf("Hello World!\n");
   printf("Ben BAker, Vadim Derun & Joel Hernandez");
-  printf(Mech. Engineer)
+  printf("Mech. Engineer");
+  printf("Francisco Padilla");
+  printf("Computer Science");
+  
   return 0;
 }
