679bcdccd745b92992802d41a35ed613419aa865
Vadim Derun <vadimderun@yahoo.com>
2019-01-17 15:31:05 -0600

    new

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Joel, Vadim & BEN
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Ben BAker, Vadim Derun & Joel Hernandez");
+  printf(Mech. Engineer)
+  return 0;
+}
